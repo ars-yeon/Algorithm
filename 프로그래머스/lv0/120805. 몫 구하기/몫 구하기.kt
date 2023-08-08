3d0dc3ed9695feb74954e5008902367b355f698d
SoyeonLee <68272722+ars-yeon@users.noreply.github.com>
2023-08-08 09:21:59 +0900

    Update 몫 구하기.kt

--- 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.kt
+++ 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.kt
@@ -1,7 +1,3 @@
 class Solution {
-    fun solution(num1: Int, num2: Int): Int {
-        var answer: Int = 0
-        answer = num1/num2
-        return answer
-    }
+    fun solution(num1: Int, num2: Int) = num1/num2
 }
