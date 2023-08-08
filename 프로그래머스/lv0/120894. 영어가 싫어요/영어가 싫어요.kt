236ecc0f7b2fb2463fb55c6814543979fa8bab3f
SoyeonLee <68272722+ars-yeon@users.noreply.github.com>
2023-08-08 09:53:05 +0900

    [level 0] Title: 영어가 싫어요, Time: 11.23 ms, Memory: 61.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(nums: String): Long {
+        val map = mapOf("zero" to 0, "one" to 1, "two" to 2, "three" to 3, "four" to "4", "five" to "5", "six" to "6", "seven" to "7", "eight" to "8", "nine" to "9")
+        
+        var res = nums
+        map.forEach {
+            res = res.replace(it.key, it.value.toString())
+        }
+        return res.toLong()
+    }
+}
