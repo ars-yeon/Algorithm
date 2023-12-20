25104b8a7895a00b7735979c74b9ba282d6c77f3
SoyeonLee <68272722+ars-yeon@users.noreply.github.com>
2023-12-20 21:06:05 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 59.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.kt
@@ -0,0 +1,6 @@
+class Solution {
+    fun solution(a: Int, b: Int, c: Int): Int =
+    if (a==b && b==c) (a+b+c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c)
+    else if (a==b || b==c || a==c) (a+b+c) * (a*a + b*b + c*c)
+    else a+b+c
+}
