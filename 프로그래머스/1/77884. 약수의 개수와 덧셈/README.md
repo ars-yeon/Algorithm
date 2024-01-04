ba1b8f946f7428c68aad3185a0dc9e27da733dd0
SoyeonLee <68272722+ars-yeon@users.noreply.github.com>
2024-01-04 20:27:58 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 6.96 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 1] 약수의 개수와 덧셈 - 77884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884) 
 
 ### 성능 요약
 
-메모리: 62.5 MB, 시간: 6.58 ms
+메모리: 62.4 MB, 시간: 6.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 15:43:4
+2024년 1월 4일 20:27:55
 
 ### 문제 설명
 
--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
@@ -1,9 +1,12 @@
 class Solution {
     fun solution(left: Int, right: Int): Int {
         var answer: Int = 0
+        // 제곱인 수는 약수가 홀수이므로 제곱을 했을때 1000 이하인 1부터 32까지의 제곱수를 가지는 리스트 생성
         val nums = (1..32).map { it * it }
         
+        // left부터 right까지의 모든 수에 대해 반복
         for (i in left..right) {
+            // 만약 i가 nums 리스트에 포함되어 있으면 -i를 더하고, 아니면 i를 더함
             answer += if (i in nums) -i else i
         }
         
