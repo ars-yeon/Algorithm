a7a031e5124eaef0a5c6282796a7b3cfac680588
SoyeonLee <68272722+ars-yeon@users.noreply.github.com>
2024-04-23 15:26:31 +0900

    Refactor code to improve readability and efficiency

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -1,9 +1,4 @@
 def solution(n):
-    divisors = set()
-    
-    for i in range(1, int(n ** 0.5) + 1):
-        if n % i == 0:
-            divisors.add(i)
-            if i != n // i:
-                divisors.add(n // i)
+    divisors = {i for i in range(1, int(n ** 0.5) + 1) if n % i == 0}
+    divisors.update({n // i for i in divisors})
     return sorted(divisors)
